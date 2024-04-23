9c1243bdc055dcf327b100fc4bd476a9a2de0b0a
Dagan3D <Butmanovdanil@gmail.com>
2024-04-23 12:15:08 +0300

    Сделал тестовый вариант

---
+++ to_IPCE.py
@@ -0,0 +1,7 @@
+import pandas as pd
+
+def table_dev(path: str) -> pd.DataFrame["time", "Wavelength", "Photocurrent", "Shutter"]:
+    pass
+
+def graphical_dev(df: pd.DataFrame["time", "Wavelength", "Photocurrent", "Shutter"]) -> pd.DataFrame["Wavelength", "Photocurrent"]:
+    pass
