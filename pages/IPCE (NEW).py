719133bbc452c8120b9a223df06cd249f4cad41f
Dagan3D <Butmanovdanil@gmail.com>
2024-04-24 16:13:07 +0300

    New_IPCE complied

---
+++ pages/IPCE (NEW).py
@@ -0,0 +1,135 @@
+import streamlit as st # type: ignore
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from io import StringIO
+import os
+import to_IPCE
+
+"""## Обработка данных со спектрофотометра для получения зависимости IPCE от длины волны"""
+#%% Калибровочный график
+
+with st.expander("Калибровочный график"):
+  
+  st.markdown('''
+    Этот обработчик был испытан со следующими параметрами на приборе:   
+              Длинны волн: 280 - 450 нм  
+              Шаг длинны волны: 5 нм  
+              Интервал меду измерениями: 50 мс  
+              Время изменения в темноте и на свету: 30 секунд  
+              Время ожидания до измерения: 120 секунд  
+              Смещение на образце: 200 мВ  
+              Чувствительность: 1 мкА  
+          ''')
+  
+  calibration_valid = False
+  uploaded_file = st.file_uploader("Файл калибровки", type = ['txt', 'csv'])
+  
+
+  if uploaded_file is not None:
+    df = to_IPCE.read_data(uploaded_file)
+    df = to_IPCE.reduction_smooth(df, window=1)
+    df = to_IPCE.time_split(df)
+    df["Photocurrent"] = df["Current"]
+    df = to_IPCE.get_photocurrent(df, window=10) 
+    if df is not None:
+      df["K_diode"] = df['Wavelength'].apply(to_IPCE.p_diode)
+      df["Мощность излучения, мкВт"] = df["Photocurrent"]/df["K_diode"]
+      calibration_valid = True
+
+      fig = px.line(df, x="Wavelength", y="Мощность излучения, мкВт")
+      st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+      if st.checkbox('Показать таблицу данных калибровки'):
+        df
+
+#%%Файлы данных
+if calibration_valid:
+  with st.expander("Файлы данных"):
+    """## Файлы данных.   
+    Тут можно загружать сразу несколько файлов.  
+    Образцам будет присвоено имя файла.
+    """
+
+    data_valid = False
+    uploaded_files = st.file_uploader("Файлы данных", type = ['txt', 'csv'], accept_multiple_files=True)
+    
+    currents_sample = pd.DataFrame()
+    currents_sample['Длина волны, нм'] = df["Wavelength"]
+    samples = []
+    for uploaded_file in uploaded_files:
+      
+      dataframe = to_IPCE.read_data(uploaded_file)
+      dataframe = to_IPCE.reduction_smooth(dataframe)
+      dataframe = to_IPCE.time_split(dataframe)
+      dataframe = to_IPCE.cut_baseline(dataframe)
+      dataframe = to_IPCE.get_photocurrent(dataframe, window=10)
+      if dataframe is not None:
+        samples.append(os.path.splitext(uploaded_file.name)[0])
+        currents_sample[samples[-1]] = dataframe["Photocurrent"]
+        
+        data_valid = True
+
+    if(data_valid):
+      fig = px.line(currents_sample.dropna(), x="Длина волны, нм", y=samples, labels={'value':"Сила тока, мкА"})
+      fig.update_layout(legend=dict(yanchor="top",xanchor="right"))
+      st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+      if st.checkbox('Показать таблицу исходных фототоков'):
+        currents_sample
+
+#%% Учёт площади образца
+  if data_valid:
+    with st.expander("Учёт площади образца"):
+      "## Пересчёт фототока в плотность фототока для учёта различной площади образца"
+      st.markdown(r'''$I_{удельный} =\frac{I_{образца}}{S_{образца}}$''')
+      area_sample = pd.DataFrame()
+      area_sample["Образец"] = samples
+      area_sample["Площадь образца, см^2"] = 7.0
+      area_sample = area_sample.astype({"Площадь образца, см^2" : float})
+      area_sample = st.data_editor(area_sample)
+      
+
+      density_current = pd.DataFrame()
+      density_current['Длина волны, нм'] = currents_sample['Длина волны, нм']
+      
+      for current_sample in samples:
+        area = area_sample.loc[area_sample["Образец"] == current_sample]["Площадь образца, см^2"]
+        density_current[current_sample] = currents_sample[current_sample]/(float(area))
+        
+      fig = px.line(density_current.dropna(), x="Длина волны, нм", y=samples, labels={'value': r"Плотность тока, мкА/см^2"})
+      fig.update_layout(legend=dict(yanchor="top",xanchor="right"))
+      st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+      if st.checkbox('Показать таблицу плотности фототоков'):
+        density_current
+
+#%% Пересчёт в IPCE
+  if(data_valid):
+    with st.expander("Пересчёт в IPCE"):
+      "## Пересчёт из плотности тока в IPCE"
+      st.markdown(r'''Тут производится пересчёт величины тока в эффективность преобразования фотона в электрон, по формуле:  
+                  $IPCE =I_{удельный}~*~ \frac{1240}{\lambda}~ / ~ P_{падающая}$''')
+      IPCE_sample = pd.DataFrame()
+      IPCE_sample["Длина волны, нм"] = density_current['Длина волны, нм']
+      for current_sample in samples:
+        IPCE_sample[current_sample] = (density_current[current_sample])*1240/currents_sample["Длина волны, нм"]/df["Мощность излучения, мкВт"]*100
+      
+      fig = px.line(IPCE_sample.dropna(), x="Длина волны, нм", y=samples, labels={'value':"IPCE, %"})
+      fig.update_layout(legend=dict(yanchor="top",xanchor="right"))
+      st.plotly_chart(fig, theme="streamlit", use_container_width=True)
+      if st.checkbox('Показать таблицу IPCE'):
+        IPCE_sample
+
+      @st.cache_data
+      def convert_df(df):
+        # IMPORTANT: Cache the conversion to prevent computation on every rerun
+        return df.to_csv(sep=";", index=False).encode('cp1251')
+
+      csv = convert_df(IPCE_sample)
+      
+      st.download_button(
+        label="Скачать результат",
+        data=csv,
+        file_name='IPCE.csv',
+        mime='text/csv',
+        type="primary"
+      )
+
--- to_IPCE.py
+++ to_IPCE.py
@@ -1,7 +1,66 @@
 import pandas as pd
+import numpy as np
+from datetime import datetime
+import streamlit as st
+from statsmodels.tsa.seasonal import STL
 
-def table_dev(path: str) -> pd.DataFrame["time", "Wavelength", "Photocurrent", "Shutter"]:
-    pass
+def p_diode(x):
+  if x <= 500:
+    y = 6.6763288698E-12*x**5 - 1.3543608284E-08*x**4 + 1.0888317706E-05*x**3 - 4.3335323943E-03*x**2 + 8.5399725862E-01*x - 6.6558844853E+01
+  elif 500 <= x < 950:
+    y = 5.5585093168E-04*x - 2.0552365026E-02
+  elif 950 <= x <= 1100:
+    y = 3.1698368227E-09*x**4 - 1.2731281453E-05*x**3 + 1.9132245041E-02*x**2 - 1.2752168975E+01*x + 3.1818412914E+03
+  return y
 
-def graphical_dev(df: pd.DataFrame["time", "Wavelength", "Photocurrent", "Shutter"]) -> pd.DataFrame["Wavelength", "Photocurrent"]:
+@st.cache_data  
+def read_data(path: str) -> pd.DataFrame:
+    df = pd.read_table(path,  decimal=',')
+    format_data = "%m%d%y_%H%M%S"
+
+    df["Time"] = df.Timestamp.apply( lambda x: datetime.strptime(x, format_data))
+    df["Time"] = df["Time"] - df["Time"][0]
+    df["Time"] = df["Time"].dt.total_seconds()
+    df["Shutter"] = df.Shutter.shift(-23)
+    return df
+
+@st.cache_data
+def reduction_smooth(df: pd.DataFrame, window: int = 10):
+    rolling_mean = df['Current'].ewm(alpha=0.2, adjust=False).mean()
+    df_resampled = pd.DataFrame({'Time': df['Time'].iloc[::window],
+                                 'Current': rolling_mean[::window],
+                                 'Wavelength': df['Wavelength'].iloc[::window],
+                                 "Shutter":  df['Shutter'].iloc[::window]})
+    return df_resampled
+
+@st.cache_data
+def time_split(df_: pd.DataFrame, start_wave: int = 300, stop_wave: int = 1100) -> pd.DataFrame:
+    df = df_.copy(deep=True)
+    for i in range(int(df.Time[df.index[-1]])):
+        pattern = np.linspace(0,1,len(df[df.Time == i]))
+        df.loc[df.Time == i, "Time"] += pattern
+    df = df.loc[df.Wavelength >= start_wave][["Time", "Current", "Wavelength", "Shutter"]].copy(deep=True)
+    return df
+
+@st.cache_data
+def cut_baseline(df: pd.DataFrame, window: int = 10, measure_in_monowave: int = 800) -> pd.DataFrame:
+    df_ = df.copy(deep=True)
+    stl = STL(df_.Current.dropna(), period=int(measure_in_monowave/window))
+    res = stl.fit()
+    df_["Photocurrent"] = res.seasonal
+    return df_
+
+@st.cache_data
+def get_photocurrent(df: pd.DataFrame, window:int = 10, left_shift: int = 10, right_shift: int = 5) -> pd.DataFrame:
+    IPCE_df = pd.DataFrame([])
+    df = df.copy(deep=True)
+    df["Current"] = df["Photocurrent"].rolling(window=window).median()
+    for wavelength in df["Wavelength"].unique():
+        mono_df = df.loc[df["Wavelength"] == wavelength][left_shift:-1*right_shift]
+        photocurrent = mono_df.Current.max() - mono_df.Current.min()
+        new_row = pd.DataFrame({"Wavelength": wavelength, "Photocurrent": photocurrent}, index=[len(IPCE_df)])
+        IPCE_df = pd.concat([IPCE_df, new_row])
+    return IPCE_df
+
+def plot_graf():
     pass
